a47e57f958ad268b5b355d62e8c31ea8678dae0e
AsevedoFousys <102690036+AsevedoFousys@users.noreply.github.com>
2022-04-18 15:16:29 -0300

    primeiro commit

---
+++ src/mocks/textos.js
@@ -0,0 +1,14 @@
+const textos = {
+    boasVindas: "Olá Natalia",
+    legenda: "Encontre os produtores mais próximos a você!",
+    legendaMelhoresProdutores: "Veja os produtores com 4+ estrelas!",
+    tituloProdutores: "Produtores",
+    tituloProdutor: "Detalhe do produtor",
+    tituloCestas: "Cestas",
+    topoCesta: "Detalhe da cesta",
+    botaoComprar: "Comprar",
+    tituloItens: "Itens da cesta",
+    mensagemCompra: "Agradecemos pela compra da cesta $NOME, seu pedido chegará em breve!"
+};
+
+export default textos;
